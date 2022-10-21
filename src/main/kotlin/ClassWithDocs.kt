f9725ac72d386187c6a7478c10d1723c638285e2
jay_from_future <jay.from.future@gmail.com>
2022-10-21 12:57:10 +0200

    Project to illustrate issue in Dokka

---
+++ src/main/kotlin/ClassWithDocs.kt
@@ -0,0 +1,23 @@
+/**
+ * Javadoc for class
+ */
+class ClassWithDocs {
+
+    /**
+     * add [] because exact version can be represented as [<fails_here>]
+     */
+    private val someVal = "anything"
+
+    /**
+     * add [] because exact version can be represented as \[<it_is_ok>\]
+     */
+    private val someValFixed = "anything"
+
+
+    /**
+     * Javadoc for someFun
+     */
+    fun someFun() {
+
+    }
+}
